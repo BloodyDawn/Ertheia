9bf077c9f12d50f1fe1362fd1a5c057d56e8524d
Eugene Chipachenko <echipachenko@gmail.com>
2015-09-02 19:54:37 +0300

    small fix

--- SrcGame/dwo/gameserver/model/actor/L2Attackable.java
+++ SrcGame/dwo/gameserver/model/actor/L2Attackable.java
@@ -2288,7 +2288,7 @@ public class L2Attackable extends L2Npc
 	public String getTitle()
 	{
 		if( Config.SHOW_NPC_LVL )
-			return "Lvl " + getLevel() + (isAggressive() ? "*" : "") + " - " + super.getTitle();
+			return "Lvl " + getLevel() + (isAggressive() ? "*" : "") + super.getTitle();
 		return super.getTitle();
 	}
 
