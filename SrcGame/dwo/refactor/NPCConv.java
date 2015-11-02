1304471dc182fa3d2387063894606e1338107e84
Yevhen Chypachenko <Yevhen_Chypachenko@epam.com>
2015-11-02 14:38:38 +0200

    NPC stats update

--- SrcGame/dwo/refactor/NPCConv.java
+++ SrcGame/dwo/refactor/NPCConv.java
@@ -12,43 +12,51 @@ import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 
 public class NPCConv {
-    private static final Map<String, String> MAPPING = new HashMap<>();
+    private static final Map<String, String> KEY_MAPPING = new HashMap<>();
+    private static final Map<String, String> VALUE_MAPPING = new HashMap<>();
 
     static {
-        MAPPING.put("type", "type");
-        MAPPING.put("level", "level");
+        KEY_MAPPING.put("type", "type");
+        KEY_MAPPING.put("level", "level");
 
-        MAPPING.put("baseHpMax", "org_hp");
-        MAPPING.put("baseHpReg", "org_hp_regen");
-        MAPPING.put("baseMpMax", "org_mp");
-        MAPPING.put("baseMpReg", "org_mp_regen");
+        KEY_MAPPING.put("baseHpMax", "org_hp");
+        KEY_MAPPING.put("baseHpReg", "org_hp_regen");
+        KEY_MAPPING.put("baseMpMax", "org_mp");
+        KEY_MAPPING.put("baseMpReg", "org_mp_regen");
 
-        MAPPING.put("basePAtk", "base_physical_attack");
-        MAPPING.put("baseMAtk", "base_magic_attack");
-        MAPPING.put("basePDef", "base_defend");
-        MAPPING.put("baseMDef", "base_magic_defend");
+        KEY_MAPPING.put("basePAtk", "base_physical_attack");
+        KEY_MAPPING.put("baseMAtk", "base_magic_attack");
+        KEY_MAPPING.put("basePDef", "base_defend");
+        KEY_MAPPING.put("baseMDef", "base_magic_defend");
 
-        MAPPING.put("basePAtkSpd", "base_attack_speed");
-        MAPPING.put("baseAtkRange", "base_attack_range");
-        MAPPING.put("basePCritRate", "base_critical");
+        KEY_MAPPING.put("basePAtkSpd", "base_attack_speed");
+        KEY_MAPPING.put("baseAtkRange", "base_attack_range");
+        KEY_MAPPING.put("basePCritRate", "base_critical");
 
-        MAPPING.put("baseSTR", "str");
-        MAPPING.put("baseCON", "con");
-        MAPPING.put("baseDEX", "dex");
-        MAPPING.put("baseINT", "int");
-        MAPPING.put("baseWIT", "wit");
-        MAPPING.put("baseMEN", "men");
+        KEY_MAPPING.put("baseSTR", "str");
+        KEY_MAPPING.put("baseCON", "con");
+        KEY_MAPPING.put("baseDEX", "dex");
+        KEY_MAPPING.put("baseINT", "int");
+        KEY_MAPPING.put("baseWIT", "wit");
+        KEY_MAPPING.put("baseMEN", "men");
 
-        MAPPING.put("collision_radius", "collision_radius");
-        MAPPING.put("collision_height", "collision_height");
+        KEY_MAPPING.put("collision_radius", "collision_radius");
+        KEY_MAPPING.put("collision_height", "collision_height");
 
-        MAPPING.put("rewardExp", "exp");
-        MAPPING.put("rewardSp", "sp");
+        KEY_MAPPING.put("rewardExp", "exp");
+        KEY_MAPPING.put("rewardSp", "sp");
+
+        VALUE_MAPPING.put("Npc", "L2Npc");
+        VALUE_MAPPING.put("Warehouse", "L2Npc");
+        VALUE_MAPPING.put("NewbieGuide", "L2Npc");
+        VALUE_MAPPING.put("Merchant", "L2Npc");
+        VALUE_MAPPING.put("Warpgate", "L2Npc");
+        VALUE_MAPPING.put("Guard", "L2Guard");
     }
 
     public static void main(String[] args) throws Exception {
-        File l2s = new File("/home/alf/Desktop/33900-33999.xml");
-        File original = new File("/home/alf/Desktop/L2WT-Ertheia/Data/game/data/stats/npc/data/33900-33999.xml");
+        File l2s = new File("/home/alf/Desktop/19500-19599.xml");
+        File original = new File("/home/alf/Desktop/L2WT-Ertheia/Data/game/data/stats/npc/data/19500-19599.xml");
 
         SAXBuilder builder = new SAXBuilder();
         Document document = builder.build(l2s);
@@ -82,8 +90,10 @@ public class NPCConv {
                 case "set":
                     String k = elt.getAttributeValue("name");
                     String v = elt.getAttributeValue("value");
-                    if (MAPPING.containsKey(k))
-                        l2wt.addContent(set(MAPPING.get(k), v));
+                    if (KEY_MAPPING.containsKey(k)) {
+                        l2wt.addContent(set(KEY_MAPPING.get(k),
+                                VALUE_MAPPING.containsKey(v) ? VALUE_MAPPING.get(v) : v));
+                    }
                     break;
             }
         }
